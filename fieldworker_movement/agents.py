e4df3f7296d9030aa41b64e9437f9a0c7823aae5
Aaron Stace <aaron.stace@ons.gov.uk>
2026-07-15 18:15:29 +0100

    Added census questionnaire response:
    - LSOAs assigned a random response chance (in .csv file) before field staff even visit, which is shown on response choropleth. Uniform amongst households in an LSOA
    - Now able to toggle road network visual on/off
    - Metrics shown (knocks/interactions) are now displayed by % not absolute value
    - Colours for choropleths now more agreeable

--- fieldworker_movement/agents.py
+++ fieldworker_movement/agents.py
@@ -6,6 +6,43 @@ Aaron Stace, 08/06/2026
 """
 import mesa
 
+
+class Household(mesa.Agent):
+    """
+    Household agents, located in the model at the nearest road/path node to 
+    their actual geographical location.
+    """
+    # Start with one type of household. Maybe have another household class that inherits later on?
+    def __init__(self, model, node, lsoa, response_chance=0.5,
+                 initial_completion_rate=0.0, ongoing_completion_rate=0.0,
+                 survey_completed=False):
+
+        super().__init__(model)
+
+        self.node = node
+        self.lsoa = lsoa
+        model.grid.place_agent(self, node)
+
+        self.response_chance = response_chance
+        self.initial_completion_rate = initial_completion_rate
+        self.ongoing_completion_rate = ongoing_completion_rate
+        self.survey_completed = survey_completed
+        self.completion_step = 0 if survey_completed else None
+        self.completion_source = 'initial' if survey_completed else None
+
+    def complete_survey(self, step_number, source):
+        """
+        Mark the household's electronic survey as completed once only.
+        """
+        if self.survey_completed:
+            return False
+
+        self.survey_completed = True
+        self.completion_step = step_number
+        self.completion_source = source
+        return True
+
+
 class FieldWorker(mesa.Agent):
     """
     An agent representing a field worker. They move around the road network, 
@@ -23,10 +60,16 @@ class FieldWorker(mesa.Agent):
         Move to the nearest household node (by Euclidean distance), always
         leaving the current node regardless of knock/interaction outcome.
         """
-        household_nodes = [h.node for h in self.model.households if h.node != self.node]
-        if not household_nodes:
+        nearest = min(
+            (
+                h.node for h in self.model.households
+                if h.node != self.node and not h.survey_completed
+            ),
+            key=lambda n: (n[0] - self.node[0])**2 + (n[1] - self.node[1])**2,
+            default=None,
+        )
+        if nearest is None:
             return
-        nearest = min(household_nodes, key=lambda n: (n[0] - self.node[0])**2 + (n[1] - self.node[1])**2)
         self.model.grid.move_agent(self, nearest)
         self.node = nearest
 
@@ -59,7 +102,10 @@ class FieldWorker(mesa.Agent):
         Whole process of a field worker visiting a household: knocking, waiting
         for a response, then interacting if someone opens the door.
         """
-        candidates = self.model.node_to_households.get(self.node, [])
+        candidates = [
+            household for household in self.model.node_to_households.get(self.node, [])
+            if not household.survey_completed
+        ]
         if not candidates:
             return
         household = self.random.choice(candidates)
@@ -71,21 +117,3 @@ class FieldWorker(mesa.Agent):
 
         self.move()
         self.visit_household()
-
-
-class Household(mesa.Agent):
-    """
-    Household agents, located in the model at the nearest road/path node to 
-    their actual geographical location.
-    """
-    # Start with one type of household. Maybe have another household class that inherits later on?
-    def __init__(self, model, node, lsoa, response_chance=0.5):
-
-        super().__init__(model)
-
-        self.node = node
-        self.lsoa = lsoa
-        model.grid.place_agent(self, node)
-
-        #self.hh_sentiment = hh_sentiment
-        self.response_chance = response_chance
